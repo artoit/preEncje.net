3fb56b07daac4cf848572c759af79da60df190a4
Przemek <przemek@artoit.pl>
2022-09-21 12:52:09 +0200

    Dodanie rodzajów transakcji

--- PreEncje.net/Enums.cs
+++ PreEncje.net/Enums.cs
@@ -2,6 +2,9 @@
 {
     public enum ETypDokumentu_HandloMag { FS, PA, PAi, ZK, WZ, WZv, PZ, PZv, FZ, PW, RW, MM, FSk, FZk, RR, ZD };
 
-    public enum ERodzajTransakcjiVAT { SP_Krajowa, SP_WDT, SP_EX, SP_WTTD, SP_EXU, SP_SPTK, SP_OOs, SP_OOu, ZAK_Krajowy,
-        ZAK_WNT, ZAK_IM, ZAK_WTTN, ZAK_IMU, ZAK_OOz, ZAK_ITPU, ZAK_OODT, ZAK_WNTST, ZAK_WNTPS  }
+    public enum ERodzajTransakcjiVAT
+    {
+        SP_Krajowa, SP_WDT, SP_EX, SP_WTTD, SP_EXU, SP_SPTK, SP_OOs, SP_OOu, SP_WSTO, SP_WSTOOSS, ZAK_Krajowy,
+        ZAK_WNT, ZAK_IM, ZAK_WTTN, ZAK_IMU, ZAK_OOz, ZAK_ITPU, ZAK_OODT, ZAK_WNTST, ZAK_WNTPS
+    }
 }
